d949d59d43420d618b2615493db45a1dc8600e41
JG <jacek.grat@gmail.com>
2023-04-06 19:13:56 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container class="fill-height">
+        <v-responsive class="d-flex align-center text-center fill-height">
+
+          <MyButton color="primary" size="small">
+            Nieaktywny przycisk
+            <template #append> dodatkowa treść</template>
+          </MyButton>
+
+        </v-responsive>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import MyButton from '@/components/MyButton.vue'
+</script>
---
+++ src/components/MyButton.vue
@@ -0,0 +1,16 @@
+<template>
+      <v-btn :color="color" :size="size" class="no-text-transform">
+        <slot></slot>
+        <slot name="append"></slot>
+      </v-btn>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  color: String,
+  size: String,
+});
+</script>
+<style scope>
+.no-text-transform{text-transform: none;}
+</style>
